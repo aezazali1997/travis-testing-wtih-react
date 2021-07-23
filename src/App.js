7b60cfb2190521d3f145a131dc876cd62ada0c89
aezazali1997 <aezazali.pro@gmail.com>
2021-07-24 00:20:19 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      hello world
-    </div>
-  );
+  return <div className="App">hello world test</div>;
 }
 
 export default App;
